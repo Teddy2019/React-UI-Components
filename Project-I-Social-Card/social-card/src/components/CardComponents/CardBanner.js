2594c63e7b3090d4f1dcbf30a22c17dbc39fc1c2
Your Name <tfikrie@yahoo.com>
2019-05-29 22:35:17 -0400

    header 1st CSS completed

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 function Banner(){
     return (
-      <img src='Assets/ReactImg.png' alt='react image'></img>
+      <img src='Assets/ReactImg.png' alt='react image' className='Banner'></img>
     )
 };
 
