78db8e2fc986a1a8cb3bf3c2f57ad35b0c48190c
Your Name <tfikrie@yahoo.com>
2019-05-30 19:07:00 -0400

    Footer added

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/HeaderComponents/HeaderContainer';
-import Cardcontainer from './components/CardComponents/CardContainer'
+import Cardcontainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer'
 const App = () => {
   return (
     <div className='App'> 
       <Header />
       <Cardcontainer />
+      <Footer />
     </div>
   );
 };
